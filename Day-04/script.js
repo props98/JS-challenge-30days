cba9f39ed660afdf26fc8026a1be080d47eda38d
MaxLocal <maxlocal@mail.com>
2022-09-06 11:35:51 +0300

    Add first stuff for work

---
+++ Day-04/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const inventors = [
+    {first: 'Albert', last: 'Enstein', year: 1879, passed: 1955},
+    {first: 'Issac', last: 'Newton', year: 1643, passed: 1727},
+    {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642},
+    {first: 'Marie', last: 'Curie', year: 1867, passed: 1934},
+    {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630},
+    {first: 'Nicolas', last: 'Copernicus', year: 1473, passed: 1543},
+    {first: 'Max', last: 'Planck', year: 1858, passed: 1947},
+];
+
+
+const people = [Olivia, Emma, Charlotte, Amelia, Ava, Sophia, Isabella, Mia, Evelyn, Harper, Luna, Camila, Gianna, Elizabeth, Eleanor, Ella, Abigail, Sofia, Avery, Scarlett, Emily, Aria, Penelope, Chloe, Layla, Mila, Nora, Hazel, Madison, Ellie, Lily, Nova, Isla, Grace, Violet, Aurora, Riley, Zoey, Willow, Emilia, Stella, Zoe, Victoria, Hannah, Addison, Leah, Lucy, Eliana, Ivy, Everly, Lillian, Paisley, Elena, Naomi, Maya, Natalie, Kinsley, Delilah, Claire, Audrey, Aaliyah];
