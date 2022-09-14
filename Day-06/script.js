a26221312f10753b659d9f51c9043b21d90a78ab
MaxLocal <maxlocal@mail.com>
2022-09-14 09:29:58 +0300

    Add form and css file

--- Day-06/script.js
+++ Day-06/script.js
@@ -11,4 +11,38 @@ fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data)); 
 
-console.log(cities);
+// console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return  place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+function nemberWithCommas() {
+    return x.toString().replace(/\B(?=(\d{3}))+(?!\d))/g, ',');
+}
+
+findMatches('New');
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
