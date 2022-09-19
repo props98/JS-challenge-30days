d637385b11e968d4a5f4d147a785346e9748ab7d
MaxLocal <maxlocal@mail.com>
2022-09-19 09:37:57 +0300

    Pass lesson

--- Day-10/script.js
+++ Day-10/script.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('Hello');
+let resetButton = document.querySelector('button'); 
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+// console.log(checkboxes);
+
+// Обнуление  чекбоксов
+resetButton.addEventListener('click', () => {
+    checkboxes.forEach(checkbox => {
+        // console.log(checkbox);
+        checkbox.checked = false;
+    })
+});
+
+let lastChecked;
+
+function handleCheck(e) {
+    // Проверяем нажата ли клавиша shift
+    // И проверяем выбран ли чекбокс
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        // Иди в начало и сделай что мы просим
+        // Зациклим каждый отдельный чекбокс
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox == this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting check in between!');
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
