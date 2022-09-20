3b43be08f458f5a62cd2b9dc92c4a57cad2afb2a
MaxLocal <maxlocal@mail.com>
2022-09-20 10:03:37 +0300

    Add css file

--- Day-11/script.js
+++ Day-11/script.js
@@ -1,2 +1,49 @@
 'use strict';
 
+//* Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+//* Build out functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '▶︎' : '◼︎';
+    // console.log(icon);
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    // console.log(this.value);
+    // console.log(this.name);
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    
+}
+
+
+//* Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
