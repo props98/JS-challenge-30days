2c1b49e2bb1d332d42918da0f71c856163f35c53
MaxLocal <maxlocal@mail.com>
2022-10-10 11:17:27 +0300

    pass lesson

--- Day-26/script.js
+++ Day-26/script.js
@@ -1,6 +1,44 @@
 "use strict";
 
-const triggers = document.querySelector('.cool > li');
-const background = document.querySelector('.dropdoenBackground');
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
+// console.log(triggers); node list
+
+function handleEnter() {
+    this.classList.add('trigger-enter');
+
+    // setTimeout(() => {
+    //     if (this.classList.contains('trigger-enter')) {
+    //         this.classList.add('trigger-enter-active');
+    //     }
+    // }, 150);
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    }
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
+
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trugger-enter-actuve');
+
+    background.classList.remove('open');
+}
+
+triggers.forEach(triggers => triggers.addEventListener('mouseenter', handleEnter));
+triggers.forEach(triggers => triggers.addEventListener('mouseleave', handleLeave));
