140899e535382d9b496ebb88cf595d6a1c81e4b3
MaxLocal <maxlocal@mail.com>
2022-10-10 10:32:47 +0300

    add script

---
+++ Day-26/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const triggers = document.querySelector('.cool > li');
+const background = document.querySelector('.dropdoenBackground');
+const nav = document.querySelector('.top');
+
